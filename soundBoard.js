217743025934cfc01aedfa071c931bd79e458edd
manitofighh <manitofigh@protonmail.com>
2023-02-18 12:24:18 -0500

    Sounds now reset everytime they are clicked

--- soundBoard.js
+++ soundBoard.js
@@ -1,71 +1,30 @@
 var path = "Sounds/";
-// const android = new Audio(path + "android.mp3");
-// const augghh = new Audio(path + "augghh.mp3");
-// const slowAugh = new Audio (path + "slowAugh.mp3")
-// const hellNah = new Audio(path + "hellNah.mp3");
-// const nokia = new Audio(path + "nokia.mp3");
-// const omgb = new Audio(path + "omgb.mp3");
-// const ohio = new Audio(path + "ohio.mp3")
-// const shutcho = new Audio(path + "shutcho.mp3");
-// const slipping = new Audio(path + "slipping.mp3");
-// const tarkibi = new Audio(path + "tarkibi");
-// const wth = new Audio(path + "wth.mp3");
 const resetButton = document.querySelector('.reset');
 
 var buttonCount = document.querySelectorAll(".sound-buttons").length;
 // var audiosArray = [android, augghh, slowAugh, hellNah, nokia, omgb, ohio, shutcho, slipping, tarkibi, wth];
+var audioArray = [];
+
+function resetSound(item){
+    item.pause();
+    item.currentTime = 0;
+}
 
 for (var i = 0; i < buttonCount ; i++){
     document.querySelectorAll(".sound-buttons")[i].addEventListener("click", function(){
         var audioName = this.classList[1];
-        var audioToPlay = new Audio(path + audioName + ".mp3");
+        var audioToPlay = audioArray.find(a => a.src.includes(audioName));
+        if (!audioToPlay) {
+            audioToPlay = new Audio(path + audioName + ".mp3");
+            audioArray.push(audioToPlay);
+        }
+        resetSound(audioToPlay);
         audioToPlay.play();
     });
 }
-// document.getElementsByClassName("android")[0].addEventListener("click", function(){
-//     android.play();
-// });
 
-// document.getElementsByClassName("augghh")[0].addEventListener("click", function(){
-//     slowAugh.play();
-// });
-
-// document.getElementsByClassName("hellNah")[0].addEventListener("click", function(){
-//     hellNah.play();
-// });
-
-// document.getElementsByClassName("nokia")[0].addEventListener("click", function(){
-//     nokia.play();
-// });
-
-// document.getElementsByClassName("ohio")[0].addEventListener("click", function(){
-//     ohio.play();
-// });
-
-// document.getElementsByClassName("omgb")[0].addEventListener("click", function(){
-//     omgb.play();
-// });
-
-// document.getElementsByClassName("shutcho")[0].addEventListener("click", function(){
-//     shutcho.play();
-// });
-
-// document.getElementsByClassName("slipping")[0].addEventListener("click", function(){
-//     slipping.play();
-// });
-
-// document.getElementsByClassName("wth")[0].addEventListener("click", function(){
-//     wth.play();
-// });
-
-// function resetSound(item){
-//     item.pause()
-//     item.currentTime = 0;
-// }
-
-// resetButton.addEventListener('click', function() { // Loops through all sounds and resets them
-//     // location.reload(); (Reloads the page)
-//     for (var i of audiosArray){
-//         resetSound(i);
-//     }
-//   });
+resetButton.addEventListener("click", function() { // Loops through all sounds and resets them
+    for (var j of audioArray){
+        resetSound(j);
+    }
+  });
